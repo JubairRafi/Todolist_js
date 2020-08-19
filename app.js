dc0c55eb7273b54b5b8bbd77b850dbf379afe720
jobairRafi96 <jubairrafi96960@gmail.com>
2020-08-18 18:13:02 -0700

    ejs added

--- app.js
+++ app.js
@@ -3,4 +3,20 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req,res))
+app.use('view engine' 'ejs');
+
+app.get("/", function(req,res){
+  var today = new Date();
+
+  if (today.getDay() === 6|| today.getDay() === 0){
+    res.send("yoyyo its weekend");
+  }else{
+    res.sendFile(__dirname +"/index.html");
+  }
+});
+
+
+
+app.listen(3000,()=>{
+  console.log("ss");
+});

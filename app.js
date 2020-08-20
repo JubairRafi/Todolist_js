78102fe4eda5bf04ad63c49ef5a9a7a2cc550bff
jobairRafi96 <jubairrafi96960@gmail.com>
2020-08-19 19:42:11 -0700

    simple todo

--- app.js
+++ app.js
@@ -3,40 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items =["buy food","cok food", "eat food"];
+
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extenden:true}));
 
 app.get("/", function(req,res){
   var today = new Date();
-  var currentDay = today.getDay();
-  var day ="";
 
-  switch (currentDay) {
-    case 0:
-      day="sunday";
-      break;
-      case 1:
-        day="monday";
-        break;
-        case 2:
-          day="tuesday";
-          break;
-          case 3:
-            day="wednesday";
-            break;
-            case 4:
-              day="thursday"; 
-              break;
-              case 5:
-                day="friday";
-                break;
-                case 6:
-                  day="saturday";
-                  break;
-    default:
-    console.log("error");
-  }
+  var options ={
+    weekday:"long",
+    day: "numeric",
+    month:"long"
+  };
 
-    res.render("list", {kindofday: day });
+  var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("list", {kindofday: day, newListItems: items });
+});
+
+app.post("/",(req,res)=>{
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 
